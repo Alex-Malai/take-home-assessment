f85253a83fd1201a8c887777c3674b1ab9e1de42
alex-malai <amalai@degreed.com>
2024-11-12 12:41:46 -0500

    adding all needed

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   viewportHeight: 1500,
   viewportWidth: 1980,
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
     specPattern: 'cypress/e2e/**/*.{e2e.ts,e2e.js}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
